bed8c4b15fae6f6615c36935ad28d161760fa46c
anna-hallinder <anna.hallinder@iths.se>
2024-12-02 14:58:15 +0100

    Added maps

---
+++ MenuService.Domain/Entities/PizzaEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace MenuService.Domain.Entities;
+
+public class PizzaEntity
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public List<string> Ingredients { get; set; } = new();
+    public decimal Price { get; set; }
+}
